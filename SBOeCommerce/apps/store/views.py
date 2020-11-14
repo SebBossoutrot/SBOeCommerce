f143c6f2bf7a2ab8ef1c159c0df9cb313a26fcc1
Sebastien <sebastien@bossoutrot.com>
2020-11-14 12:31:32 +0100

    error a corriger

--- SBOeCommerce/apps/store/views.py
+++ SBOeCommerce/apps/store/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from .models import Product, Category
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
+
+
+def product_detail(request, category_slug, slug):
+    product = get_object_or_404(Product, slug=slug)
+
+    context =  {
+        'product': product
+    }
+
+    return render(request, 'product_detail.html', context)
+
+def category_detail(request, slug):
+    category = get_object_or_404(Category, slug=slug)
+    products = category.products.all() 
+
+    context =  {
+        'category': category,
+        'product': products
+    }
+
+    return render(request, 'category_detail.html', context)
