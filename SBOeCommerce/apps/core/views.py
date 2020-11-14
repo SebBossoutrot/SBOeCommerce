c9b676235d2f45ba8c1ed555bdbda3e7f873ecf4
Sebastien <sebastien@bossoutrot.com>
2020-11-14 14:51:15 +0100

    end of the part 3

--- SBOeCommerce/apps/core/views.py
+++ SBOeCommerce/apps/core/views.py
@@ -7,7 +7,7 @@ from apps.store.models import Product
 # Create your views here.
 
 def frontpage(request):
-    products = Product.objects.all()
+    products = Product.objects.filter(is_featured=True)
 
     context = {
         'products': products
@@ -17,3 +17,6 @@ def frontpage(request):
 
 def contact(request):
     return render(request, 'contact.html')
+
+def about(request):
+    return render(request, 'about.html')
--- SBOeCommerce/apps/store/models.py
+++ SBOeCommerce/apps/store/models.py
@@ -6,9 +6,11 @@ from django.db import models
 class Category(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255)
+    ordering = models.IntegerField(default=0)
 
     class Meta:
         verbose_name_plural = 'Categories'
+        ordering = ('ordering',)
 
     def __str__(self):
         return self.title
@@ -21,9 +23,12 @@ class Product(models.Model):
     slug = models.SlugField(max_length=255)
     description = models.TextField(blank=True, null=True)
     price = models.FloatField()
+    is_featured = models.BooleanField(default=False)
+    date_added = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         verbose_name_plural = 'Products'
+        ordering = ('-date_added',)
 
     def __str__(self):
         return self.title
